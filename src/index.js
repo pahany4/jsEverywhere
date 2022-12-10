6a3dddddf65b091cda95f0246d7895dc31aad055
paha <pahany4@mail.ru>
2022-12-10 17:12:10 +0700

    Подключение БД.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 // index.js
 // This is the main entry point of our application
 
+require('dotenv').config();
+const db = require('./db');
+const models = require('./models');
 const express = require('express');
 const {ApolloServer, gql} = require('apollo-server-express');
 // Запускаем сервер на порте, указанном в файле .env, или на порте 4000
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
 
-let notes = [
+/*let notes = [
   {id: '1', content: 'This is a note', author: 'Adam Scott'},
   {id: '2', content: 'This is another note', author: 'Harlow Everly'},
   {id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison'}
-];
+];*/
 
 // Строим схему с помощью языка схем GraphQL
 const typeDefs = gql`
@@ -42,25 +46,28 @@ type Mutation {
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
-    notes: () => notes,
-    note: (parent, args) => {
-      return notes.find(note => note.id === args.id);
+    notes: async () => {
+      return await models.Note.find();
+    },
+    note: async (parent, args) => {
+      return await models.Note.findById(args.id);
     }
   },
   Mutation: {
-    newNote: (parent, args) => {
-      let noteValue = {
-        id: String(notes.length + 1),
+    newNote: async (parent, args) => {
+      return await models.Note.create({
         content: args.content,
         author: 'Adam Scott'
-      };
-      notes.push(noteValue);
-      return noteValue;
+      });
     }
   }
 };
 
 const app = express();
+
+// Подключаем БД
+db.connect(DB_HOST);
+
 // Настраиваем Apollo Server
 const server = new ApolloServer({typeDefs, resolvers});
 // Применяем промежуточное ПО Apollo GraphQL и указываем путь к /api
---
+++ src/models/note.js
@@ -0,0 +1,23 @@
+// Запрашиваем библиотеку mongoose
+const mongoose = require('mongoose');
+// Определяем схему БД заметки
+const noteSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    }
+  },
+  {
+// Присваиваем поля createdAt и updatedAt с типом данных
+    timestamps: true
+  }
+);
+// Определяем модель 'Note' со схемой
+const Note = mongoose.model('Note', noteSchema);
+// Экспортируем модуль
+module.exports = Note;
